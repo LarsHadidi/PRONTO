4193dbf03fd65bbf0cc5c2234beb3b8b2f3e8626
Lars Hadidi <lars.hadidi>
2022-12-17 22:39:38 +0100

    feat: Database Mapper
    Database mapper classes generated from PonyORM

--- src/database.py
+++ src/database.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from pony.orm import *
 import os
 
 
@@ -7,16 +8,62 @@ db.bind(provider = 'postgres', user = os.getenv('DB_USER'), password = os.getenv
 
 
 class Participant(db.Entity):
-    pass
+    id = PrimaryKey(int, auto=True)
+    first_name = Required(str, 128)
+    last_name = Required(str, 128)
+    email_address = Required(str, 512, unique=True)
+    phone_number = Optional(str, 32, nullable=True)
+    accessibility = Required(bool)
+    location = Required('Location')
+    objections = Required('Objections')
+    allergies = Required('Allergies')
 
 
 class Location(db.Entity):
-    pass
+    id = PrimaryKey(int, auto=True)
+    participants = Set(Participant)
+    lat = Required(Decimal)
+    lon = Required(Decimal)
+    postal_address = Required(str, 512)
+    capacity = Required(int)
+    accessibility = Required(bool)
+    pets = Required(bool)
 
 
 class Objections(db.Entity):
-    pass
+    id = PrimaryKey(int, auto=True)
+    participant = Optional(Participant)
+    animal = Required(bool)
+    meat = Required(bool)
+    beef = Required(bool)
+    pork = Required(bool)
+    poultry = Required(bool)
+    fish = Required(bool)
+    seafood = Required(bool)
+    dairy = Required(bool)
+    eggs = Required(bool)
+    honey = Required(bool)
+    gelatine = Required(bool)
 
 
 class Allergies(db.Entity):
-    pass
+    id = PrimaryKey(int, auto=True)
+    participant = Optional(Participant)
+    peanut = Required(bool)
+    fish = Required(bool)
+    celiac = Required(bool)
+    gluten = Required(bool)
+    wheat = Required(bool)
+    shellfish = Required(bool)
+    lupin = Required(bool)
+    lactose = Required(bool)
+    milk = Required(bool)
+    nut = Required(bool)
+    celery = Required(bool)
+    mustard = Required(bool)
+    sesame = Required(bool)
+    soy = Required(bool)
+    pet = Required(bool)
+
+
+db.generate_mapping(create_tables = False)
