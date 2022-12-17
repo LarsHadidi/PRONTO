c736af71e53b29781f9a18f95b0b3d03b3b4c829
Lars Hadidi <lars.hadidi>
2022-12-17 17:19:51 +0100

    feat: geocoding API
    Using locationIQ as API.

--- src/geocoder.py
+++ src/geocoder.py
@@ -1,15 +1,17 @@
 from decimal import Decimal
 from typing import List
+import requests
 import os
 
+import location
 
-base_url = os.getenv('GC_URL')
+base_url = "https://eu1.locationiq.com"
 api_key = os.getenv('GC_KEY')
 
 
 def encode(postal_address: str) -> (Decimal, Decimal):
     pass
-
+    
 
 def decode(lat: Decimal, lon: Decimal) -> str:
     pass
