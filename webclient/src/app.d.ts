ac6627aec35d1a9465801548f25cbc8f3057a597
Lars Hadidi <lars.hadidi@gmail.com>
2024-02-08 16:24:32 +0000

    dev: scaffolding [SvelteKit] application

---
+++ webclient/src/app.d.ts
@@ -0,0 +1,10 @@
+declare global {
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface PageState {}
+    // interface Platform {}
+  }
+}
+export {};
