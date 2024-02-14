df6208786fbb570adc00cb7133ddce2224695e95
Lars Hadidi <lars.hadidi@gmail.com>
2024-02-14 19:29:02 +0000

    conf: create configuration file for [TailwindCSS]

---
+++ ui-components/tailwind.config.ts
@@ -0,0 +1,28 @@
+
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
+
+import { skeleton } from '@skeletonlabs/tw-plugin';
+
+const config = {
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve(
+			'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		// Append the Skeleton plugin after other plugins
+		skeleton({
+      themes: { preset: [ { name: "hamlindigo", enhancements: true } ] }
+    })
+	]
+} satisfies Config;
+
+export default config;
+						
