53bfac9a886dcd0e11f54288009cb2cfbde0e050
Lars Hadidi <lars.hadidi@gmail.com>
2024-02-22 14:05:59 +0000

    dev: create ColorPicker component

---
+++ ui-components/src/lib/svelte-components/ColorPicker.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	export let name: string;
+	export let value: string = $page.data?.[name] ?? '';
+</script>
+
+<input {name} type="color" class="input" bind:value>
---
+++ ui-components/src/stories/Components/ColorPicker.stories.svelte
@@ -0,0 +1,26 @@
+<script lang="ts" context="module">
+  import { Story } from '@storybook/addon-svelte-csf';
+  import type { Meta } from '@storybook/svelte';
+  import ColorPicker from '../../lib/svelte-components/ColorPicker.svelte';
+
+  export const meta: Meta<ColorPicker> = {
+    component: ColorPicker,
+    tags: ['autodocs'],
+    parameters: {
+      fetchMock: { },
+      sveltekit_experimental: {
+        stores: {
+          page: {
+            data: {
+              'example-data': "#A8BEF1"
+            }
+          }
+        }
+      }
+    }
+  };
+</script>
+
+<Story name="Default">
+  <ColorPicker name="example-data"></ColorPicker>
+</Story>
