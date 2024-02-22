a3c9ba5685f000a187daf8936b8953e5cb5150f5
Lars Hadidi <lars.hadidi@gmail.com>
2024-02-22 14:07:18 +0000

    dev: create NumberInput component

---
+++ ui-components/src/stories/Components/NumberInput.stories.svelte
@@ -0,0 +1,26 @@
+<script lang="ts" context="module">
+  import { Story } from '@storybook/addon-svelte-csf';
+  import type { Meta } from '@storybook/svelte';
+  import NumberInput from '../../lib/svelte-components/NumberInput.svelte';
+
+  export const meta: Meta<NumberInput> = {
+    component: NumberInput,
+    tags: ['autodocs'],
+    parameters: {
+      fetchMock: { },
+      sveltekit_experimental: {
+        stores: {
+          page: {
+            data: {
+              'example-data': "10"
+            }
+          }
+        }
+      }
+    }
+  };
+</script>
+
+<Story name="Default">
+  <NumberInput name="example-data"></NumberInput>
+</Story>
