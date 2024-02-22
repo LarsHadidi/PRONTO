1e3382434c7f8d4ac0f66e89f9cf8b48ed7c7284
Lars Hadidi <lars.hadidi@gmail.com>
2024-02-22 14:06:22 +0000

    dev: create DatePicker component

---
+++ ui-components/src/lib/svelte-components/DatePicker.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+	export let name: string;
+	export let value: string = $page.data?.[name]?.toISOString().split('T')[0];
+</script>
+
+<input {name} type="date" class="input" bind:value>
---
+++ ui-components/src/stories/Components/DatePicker.stories.svelte
@@ -0,0 +1,26 @@
+<script lang="ts" context="module">
+  import { Story } from '@storybook/addon-svelte-csf';
+  import type { Meta } from '@storybook/svelte';
+  import DatePicker from '../../lib/svelte-components/DatePicker.svelte';
+
+  export const meta: Meta<DatePicker> = {
+    component: DatePicker,
+    tags: ['autodocs'],
+    parameters: {
+      fetchMock: { },
+      sveltekit_experimental: {
+        stores: {
+          page: {
+            data: {
+              'example-data': new Date()
+            }
+          }
+        }
+      }
+    }
+  };
+</script>
+
+<Story name="Default">
+  <DatePicker name="example-data"></DatePicker>
+</Story>
