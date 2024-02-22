de0ac81695232c5a7ef4a9af22bda43ad1cee93b
Lars Hadidi <lars.hadidi@gmail.com>
2024-02-22 14:11:02 +0000

    dev: create Card component

---
+++ ui-components/src/lib/svelte-components/Card.svelte
@@ -0,0 +1,3 @@
+<div class="{`card p-4`}">
+	<slot></slot>
+</div>
---
+++ ui-components/src/stories/Components/Card.stories.svelte
@@ -0,0 +1,21 @@
+<script lang="ts" context="module">
+  import { Story } from '@storybook/addon-svelte-csf';
+  import type { Meta } from '@storybook/svelte';
+  import Card from '../../lib/svelte-components/Card.svelte';
+
+  export const meta: Meta<Card> = {
+    component: Card,
+    tags: ['autodocs'],
+    parameters: {
+      fetchMock: { }
+    }
+};
+</script>
+
+<Story name="Default">
+  <Card>
+    <b>Card</b>
+    <br/>
+    I am a card
+  </Card>
+</Story>
