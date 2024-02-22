229a70485a025626a3563fd07cc1946b2ba3cbc8
Lars Hadidi <lars.hadidi@gmail.com>
2024-02-22 14:05:33 +0000

    dev: create ListBox component

---
+++ ui-components/src/lib/svelte-components/ListBox.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
+	export let items: {value: unknown; label: string}[];
+	export let name: string;
+	export let multiple: boolean;
+
+	let values: string[]|string = $page.data?.[name] ?? [];
+	values = Array.isArray(values) ? values.map(String) : values.toString();
+</script>
+
+<ListBox {name} {multiple}>
+	{#each items as item}
+		<ListBoxItem bind:group={values} name={name} value={item.value}>{item.label}</ListBoxItem>
+	{/each}
+</ListBox>
---
+++ ui-components/src/stories/Components/ListBox.stories.svelte
@@ -0,0 +1,52 @@
+<script lang="ts" context="module">
+  import { Story, Template } from '@storybook/addon-svelte-csf';
+  import type { Meta } from '@storybook/svelte';
+  import ListBox from '../../lib/svelte-components/ListBox.svelte';
+
+  export const meta: Meta<ListBox> = {
+    component: ListBox,
+    tags: ['autodocs'],
+    parameters: {
+      fetchMock: { },
+      sveltekit_experimental: {
+        stores: {
+          page: {
+            data: {
+                'example-data': ['H']
+            }
+          }
+        }
+      }
+    },
+    argTypes: {
+      items: {
+        description: "Array of objects representing available items.",
+        control: { type: 'array' }
+      }
+    }
+  };
+</script>
+
+<Template let:args>
+  <ListBox {...args}></ListBox>
+</Template>
+
+<Story name="Default" args={ 
+  {name: "example-data", multiple: true, items:
+  [
+    {label: "Hydrogen", "value": "H"},
+    {label: "Helium", "value": "He"},
+    {label: "Lithium", "value": "Li"},
+    {label: "Beryllium", "value": "Be"},
+    {label: "Sodium", "value": "Na"},
+    {label: "Magnesium", "value": "Mg"},
+    {label: "Potassium", "value": "K"},
+    {label: "Calcium", "value": "Ca"},
+    {label: "Rubidium", "value": "Rb"},
+    {label: "Strontium", "value": "Sr"},
+    {label: "Cesium", "value": "Cs"},
+    {label: "Barium", "value": "Ba"},
+    {label: "Francium", "value": "Fr"},
+    {label: "Radium", "value": "Ra"}
+  ]} 
+}/>
