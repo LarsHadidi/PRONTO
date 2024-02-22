7aa6cb0eb832c2457a46baff9a2399d55494260d
Lars Hadidi <lars.hadidi@gmail.com>
2024-02-22 14:17:24 +0000

    dev: create Rating component

---
+++ ui-components/src/lib/svelte-components/Rating.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { Ratings } from '@skeletonlabs/skeleton';
+	import { page } from '$app/stores';
+	export let name: string;
+	export let max: number;
+	export let value: number = $page.data?.[name] ?? 0;
+
+	function iconClick(event: CustomEvent<{index:number}>): void {
+		value = event.detail.index;
+	}
+</script>
+
+<Ratings {name} {max} bind:value interactive on:icon={iconClick}>
+	<svelte:fragment slot="empty">◯</svelte:fragment>
+    <svelte:fragment slot="full">⚫</svelte:fragment>
+</Ratings>
