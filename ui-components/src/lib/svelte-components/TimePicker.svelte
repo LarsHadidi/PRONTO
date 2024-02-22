ae6eac6ec669050faabeebc790d134d234bd63ad
Lars Hadidi <lars.hadidi@gmail.com>
2024-02-22 14:06:51 +0000

    dev: create TimePicker component

---
+++ ui-components/src/lib/svelte-components/TimePicker.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+	export let name: string;
+	export let value: string = $page.data?.[name]?.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+</script>
+
+<input name={name} type="time" class="input" bind:value>
---
+++ ui-components/src/stories/Components/TimePicker.stories.svelte
@@ -0,0 +1,26 @@
+<script lang="ts" context="module">
+  import { Story } from '@storybook/addon-svelte-csf';
+  import type { Meta } from '@storybook/svelte';
+  import TimePicker from '../../lib/svelte-components/TimePicker.svelte';
+
+  export const meta: Meta<TimePicker> = {
+    component: TimePicker,
+    tags: ['autodocs'],
+    parameters: {
+      fetchMock: { },
+      sveltekit_experimental: {
+        stores: {
+          page: {
+            data: {
+              'example-data': new Date()
+            }
+          }
+        }
+      }
+    }
+  };
+</script>
+
+<Story name="Default">
+  <TimePicker name="example-data"></TimePicker>
+</Story>
