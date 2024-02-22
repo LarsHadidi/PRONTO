440371a0ecd64a71f6895d9bbe133104707eec99
Lars Hadidi <lars.hadidi@gmail.com>
2024-02-22 14:18:13 +0000

    dev: create Toggle component

---
+++ ui-components/src/stories/Components/Toggle.stories.svelte
@@ -0,0 +1,34 @@
+<script lang="ts" context="module">
+  import { Story, Template } from '@storybook/addon-svelte-csf';
+  import type { Meta } from '@storybook/svelte';
+  import Toggle from '../../lib/svelte-components/Toggle.svelte';
+
+  export const meta: Meta<Toggle> = {
+    component: Toggle,
+    tags: ['autodocs'],
+    parameters: {
+      fetchMock: { },
+      sveltekit_experimental: {
+        stores: {
+          page: {
+            data: {
+              'example-data': true
+            }
+          }
+        }
+      }
+    },
+    argTypes: {
+      label: {
+        description: "Label for the Toggle to be displayed.",
+        control: { type: 'text' }
+      }
+    }
+  };
+</script>
+
+<Template let:args>
+  <Toggle {...args}></Toggle>
+</Template>
+
+<Story name="Default" args={ {name: "example-data", label: "Light-Switch"} }/>
