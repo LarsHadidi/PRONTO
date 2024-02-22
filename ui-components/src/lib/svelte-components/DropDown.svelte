6dc7e5bb19bc3dcf22f02668364299f9cf7365d9
Lars Hadidi <lars.hadidi@gmail.com>
2024-02-22 14:00:25 +0000

    dev: create DropDown component

---
+++ ui-components/src/lib/svelte-components/DropDown.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	export let items: {value: unknown; label: string}[];
+	export let name: string;
+	export let value: string[] = $page.data?.[name];
+</script>
+
+<select {name} class="select" bind:value>
+	{#each items as item}
+		<option value={item.value}>{item.label}</option>
+	{/each}
+</select>
---
+++ ui-components/src/stories/Components/DropDown.stories.svelte
@@ -0,0 +1,52 @@
+<script lang="ts" context="module">
+  import { Story, Template } from '@storybook/addon-svelte-csf';
+  import type { Meta } from '@storybook/svelte';
+  import DropDown from '../../lib/svelte-components/DropDown.svelte';
+
+  export const meta: Meta<DropDown> = {
+    component: DropDown,
+    tags: ['autodocs'],
+    parameters: {
+      fetchMock: { },
+      sveltekit_experimental: {
+        stores: {
+          page: {
+            data: {
+              'example-data': 'Li'
+            }
+          }
+        }
+      }
+    },
+    argTypes: {
+      items: {
+        description: "Array of objects representing available items.",
+        control: { type: 'array' }
+      }
+    }
+  };
+</script>
+
+<Template let:args>
+  <DropDown {...args}></DropDown>
+</Template>
+
+<Story name="Default" args={ 
+  {name: "example-data", items: 
+  [
+    {label: "Hydrogen", "value": "H"},
+    {label: "Helium", "value": "He"},
+    {label: "Lithium", "value": "Li"},
+    {label: "Beryllium", "value": "Be"},
+    {label: "Sodium", "value": "Na"},
+    {label: "Magnesium", "value": "Mg"},
+    {label: "Potassium", "value": "K"},
+    {label: "Calcium", "value": "Ca"},
+    {label: "Rubidium", "value": "Rb"},
+    {label: "Strontium", "value": "Sr"},
+    {label: "Cesium", "value": "Cs"},
+    {label: "Barium", "value": "Ba"},
+    {label: "Francium", "value": "Fr"},
+    {label: "Radium", "value": "Ra"}
+  ]} 
+}/>
