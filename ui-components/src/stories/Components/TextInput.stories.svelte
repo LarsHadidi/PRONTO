748877e9003c01dde9e44863552288b50dece92a
Lars Hadidi <lars.hadidi@gmail.com>
2024-02-22 14:11:32 +0000

    dev: create TextInput component

---
+++ ui-components/src/stories/Components/TextInput.stories.svelte
@@ -0,0 +1,26 @@
+<script lang="ts" context="module">
+  import { Story } from '@storybook/addon-svelte-csf';
+  import type { Meta } from '@storybook/svelte';
+  import TextInput from '../../lib/svelte-components/TextInput.svelte';
+
+  export const meta: Meta<TextInput> = {
+    component: TextInput,
+    tags: ['autodocs'],
+    parameters: {
+      fetchMock: { },
+      sveltekit_experimental: {
+        stores: {
+          page: {
+            data: {
+              'example-data': "Example Text"
+            }
+          }
+        }
+      }
+    }
+  };
+</script>
+
+<Story name="Default">
+  <TextInput name="example-data"></TextInput>
+</Story>
