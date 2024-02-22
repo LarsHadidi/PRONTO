e7fa24f033739e5267d8c9d5c08ea5c0ec4d74fb
Lars Hadidi <lars.hadidi@gmail.com>
2024-02-22 14:12:10 +0000

    dev: create RangeSlider component

---
+++ ui-components/src/lib/svelte-components/RangeSlider.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { RangeSlider } from '@skeletonlabs/skeleton';
+	export let name: string;
+	export let min: number;
+	export let max: number;
+	export let step: number;
+	export let value: number = $page.data?.[name];
+</script>
+
+<RangeSlider {name} {min} {max} {step} bind:value ticked />
---
+++ ui-components/src/stories/Components/RangeSlider.stories.svelte
@@ -0,0 +1,42 @@
+<script lang="ts" context="module">
+  import { Story, Template } from '@storybook/addon-svelte-csf';
+  import type { Meta } from '@storybook/svelte';
+  import RangeSlider from '../../lib/svelte-components/RangeSlider.svelte';
+
+  export const meta: Meta<RangeSlider> = {
+    component: RangeSlider,
+    tags: ['autodocs'],
+    parameters: {
+      fetchMock: { },
+      sveltekit_experimental: {
+        stores: {
+          page: {
+            data: {
+              'example-data': '10'
+            }
+          }
+        }
+      }
+    },
+    argTypes: {
+      min: {
+        description: "Minimum value of the slider's range.",
+        control: { type: 'number' }
+      },
+      max: {
+        description: "Maximum value of the slider's range.",
+        control: { type: 'number' }
+      },
+      step: {
+        description: "Step offset.",
+        control: { type: 'number' }
+      }
+    }
+  };
+</script>
+
+<Template let:args>
+  <RangeSlider {...args}></RangeSlider>
+</Template>
+
+<Story name="Default" args={ {name: "example-data", min: 5, max: 15, step: 5} }/>
